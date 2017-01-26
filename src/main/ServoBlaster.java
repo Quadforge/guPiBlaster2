5971e278e787d5841d9b1b1f0657d178883d4d04
LoopyELBARTO <loopyelbarto@gmail.com>
2017-01-26 17:21:14 -0500

    LED flashes for half a second intervals

--- src/main/ServoBlaster.java
+++ src/main/ServoBlaster.java
@@ -1,5 +1,32 @@
 package main;
 
+import com.pi4j.io.gpio.*;
+
 public class ServoBlaster {
 
+    public static void main (String[] args) throws InterruptedException {
+        final GpioController GPIO;
+        final GpioPinDigitalOutput PIN;
+
+        GPIO = GpioFactory.getInstance();
+        PIN = GPIO.provisionDigitalOutputPin(RaspiPin.GPIO_07, "PinLED", PinState.HIGH);
+
+        Thread.sleep(500);
+
+        PIN.low();
+
+        Thread.sleep(500);
+
+        PIN.pulse(500, true);
+
+        Thread.sleep(500);
+
+        PIN.pulse(500, true);
+
+        Thread.sleep(500);
+
+        PIN.pulse(500, true);
+
+        GPIO.shutdown();
+    }
 }
