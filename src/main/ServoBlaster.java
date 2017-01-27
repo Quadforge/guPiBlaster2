1a3a5cc34881ad96c2ebc93dd0ef9f0c3431e692
LoopyELBARTO <loopyelbarto@gmail.com>
2017-01-26 19:32:52 -0500

    LED Would light up 5 times for 1 second

--- src/main/ServoBlaster.java
+++ src/main/ServoBlaster.java
@@ -4,14 +4,14 @@ import com.pi4j.io.gpio.*;
 
 public class ServoBlaster {
 
-    public static void main (String[] args) throws InterruptedException {
+    /*public static void main (String[] args) throws InterruptedException {
         final GpioController GPIO;
         final GpioPinDigitalOutput PIN;
 
         GPIO = GpioFactory.getInstance();
         PIN = GPIO.provisionDigitalOutputPin(RaspiPin.GPIO_07, "PinLED", PinState.HIGH);
 
-        Thread.sleep(1000);
+        Thread.sleep(500);
 
         PIN.low();
 
@@ -23,5 +23,34 @@ public class ServoBlaster {
         }
 
         GPIO.shutdown();
+    }*/
+    private long pulse;
+
+    public ServoBlaster(long pulse) throws InterruptedException {
+        final GpioController GPIO;
+        final GpioPinDigitalOutput PIN;
+
+        GPIO = GpioFactory.getInstance();
+        PIN = GPIO.provisionDigitalOutputPin(RaspiPin.GPIO_07, "PinLED", PinState.HIGH);
+
+        Thread.sleep(pulse);
+
+        PIN.low();
+
+        for (int i = 0; i <=3; i++){        // This would make the LED Flash 5 times
+
+            Thread.sleep(pulse);
+
+            PIN.pulse(pulse, true);
+        }
+        GPIO.shutdown();
+    }
+
+    public void setPulse(long setP){
+        pulse = setP;
+    }
+    public long getPulse(long getP){
+        pulse = getP;
+        return this.pulse;
     }
 }
--- test/ServoBlasterTest.java
+++ test/ServoBlasterTest.java
@@ -10,7 +10,7 @@ import main.ServoBlaster;
 public class ServoBlasterTest {
     @Test
     public void setPulseWidth() throws Exception {
-        ServoBlaster blaster = new ServoBlaster();
+        ServoBlaster blaster = new ServoBlaster(1000);
         //assertEquals("0=1000", blaster.setPulseWidth(), "String is not in the proper format");
     }
 }
