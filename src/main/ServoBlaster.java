3c57e18346a645824c35b06d14faaf300b623aae
LoopyELBARTO <loopyelbarto@gmail.com>
2017-02-02 14:35:14 -0500

    LED Would light up

--- src/main/ServoBlaster.java
+++ src/main/ServoBlaster.java
@@ -25,20 +25,23 @@ public class ServoBlaster {
         GPIO.shutdown();
     }*/
     private long pulse;
+    private long waitTime;
 
-    public ServoBlaster(long pulse) throws InterruptedException {
+    public ServoBlaster(long pulse, long waitTime) throws InterruptedException {
         final GpioController GPIO;
         final GpioPinDigitalOutput PIN;
 
         GPIO = GpioFactory.getInstance();
         PIN = GPIO.provisionDigitalOutputPin(RaspiPin.GPIO_07, "PinLED", PinState.HIGH);
 
+        //Thread.sleep(waitTime);
         Thread.sleep(pulse);
 
         PIN.low();
 
-        for (int i = 0; i <=3; i++){        // This would make the LED Flash 5 times
+        for (int i = 0; i <=100000; i++){        // This would make the LED Flash 5 times
 
+            //Thread.sleep(waitTime);
             Thread.sleep(pulse);
 
             PIN.pulse(pulse, true);
@@ -52,5 +55,12 @@ public class ServoBlaster {
     public long getPulse(long getP){
         pulse = getP;
         return this.pulse;
+    }
+    public void setWait(long setW){
+        waitTime = setW;
+    }
+    public long getWait(long getW){
+        waitTime = getW;
+        return this.waitTime;
     }
 }
