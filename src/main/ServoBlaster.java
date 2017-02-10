aa7b8630ac1b362c2c89baebc2d9c7f414b73b6e
LoopyELBARTO <loopyelbarto@gmail.com>
2017-02-09 22:55:08 -0500

    Changeable pulses from 1 second to 2 second with a 0.1 second increment. (Tested for LED, parameters need to be change for oscilloscope in units of milliseconds)

--- src/main/ServoBlaster.java
+++ src/main/ServoBlaster.java
@@ -1,6 +1,7 @@
 package main;
 
 import com.pi4j.io.gpio.*;
+import com.pi4j.io.spi.SpiDevice;
 
 
 public class ServoBlaster {
@@ -8,26 +9,47 @@ public class ServoBlaster {
     private long pulseWidth;
     private long waitTime;
 
-    private int[] intervals = {1000, 1100, 1200, 1300, 1400, 1500,
-                                1600, 1700, 1800, 1900, 2000};
+    private double[] intervals = {1, 1.1, 1.2, 1.3, 1.4, 1.5,
+                                1.6, 1.7, 1.8, 1.9, 2};
 
     /*public ServoBlaster(long pulseWidth, long waitTime) throws InterruptedException {*/
     public ServoBlaster(long waitTime) throws InterruptedException {
         final GpioController GPIO;
         final GpioPinDigitalOutput PIN;
 
+
         GPIO = GpioFactory.getInstance();
-        PIN = GPIO.provisionDigitalOutputPin(RaspiPin.GPIO_07, "PinLED", PinState.HIGH);
+        PIN = GPIO.provisionDigitalOutputPin(RaspiPin.GPIO_01, "PinLED", PinState.HIGH);
 
-        //PIN.low();
 
-        for (int j = 0; j < intervals.length; j++){                 //This loop use the interval array
-                pulseWidth = intervals[j];                          //setting pulseWidth equal to interval from 0-11, index
+        PIN.low();
+
+        /*for (int j = 0; j < intervals.length; j++){                 //This loop use the interval array
+                pulseWidth = (long) intervals[j];                          //setting pulseWidth equal to interval from 0-11, index
                 Thread.sleep(waitTime);                             //Turn off the LED
-                /*System.out.println(pulseWidth/1000.00);*/
+                //System.out.println(pulseWidth/1000.00);
                 System.out.println(pulseWidth/1000.00 + " sec");    //To Display the current measure of time which is seconds
                 PIN.pulse(pulseWidth, true);               //Grab the input parameter from ServoBlasterTest, then passes
-                                                                    //though loop.
+                PIN.low();                                                    //though loop.
+                Thread.sleep(waitTime);
+        }*/
+        for (int i = 1; i < 5; i ++) {
+
+
+            /*PIN.high();
+            Thread.sleep((long) 1.0);
+            PIN.low();
+            Thread.sleep(1000);*/
+
+            PIN.high();
+            Thread.sleep((long) 0.15);
+            PIN.low();
+            Thread.sleep(1000);
+
+            /*PIN.high();
+            Thread.sleep((long) 2.0);
+            PIN.low();
+            Thread.sleep(1000);*/
         }
         GPIO.shutdown();
     }
