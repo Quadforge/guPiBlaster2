bb93c90338f8897320f1c61277051498e8e4d5aa
CairaLynn <caira.borchers@yahoo.com>
2017-04-21 14:18:51 -0400

    Made an autorun feature that uses timer to send out one pulsewidth incremented at .1 ms every few seconds. Also made a function that allows userinput for the pulsewidth.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,22 @@
 package main;
 
+import com.pi4j.io.i2c.I2CFactory;
+
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] a) throws InterruptedException, IOException {
 
-        ServoBlaster3 blaster = new ServoBlaster3();
-        blaster.automatic();
+        //ServoBlaster3 blaster = new ServoBlaster3();
+        // blaster.automatic();
+        ads ADT = new ads();
+
+
+        try {
+            ADT.ADs();
+
+        } catch (I2CFactory.UnsupportedBusNumberException e) {
+            e.printStackTrace();
+        }
     }
 }
