4d098b699b38a2574d70417812ed4bc9a534ae90
LoopyELBARTO <loopyelbarto@gmail.com>
2017-11-03 16:39:58 -0400

    Reconfiguring ADS to be read properly by programmers

--- src/main/java/controllers/ADS/ADSDriver.java
+++ src/main/java/controllers/ADS/ADSDriver.java
@@ -9,10 +9,12 @@ import java.io.IOException;
 public class ADSDriver {
 
     public static void main(String[] args) throws IOException, I2CFactory.UnsupportedBusNumberException, InterruptedException {
-        ADSReadVoltage ADS = new ADSReadVoltage();
+        /*ADSReadVoltage ADS = new ADSReadVoltage();
         ADS.analogPinValueListener();
         ADS.diffAnalogInputs[0].addListener(ADS.listener);
         ADS.diffAnalogInputs[1].addListener(ADS.listener);
-        System.out.println(ADS.df.format(ADS.voltage));
+        System.out.println(ADS.df.format(ADS.voltage));*/
+        ADSVoltage ads = new ADSVoltage();
+        ads.DifferentialReadings();
     }
 }
