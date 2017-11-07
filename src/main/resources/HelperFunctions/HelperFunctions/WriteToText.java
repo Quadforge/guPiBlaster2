ba0abf849819cd4ad6aadab639f012f463115d40
NkemOhanenye <nohanenye3527@students.mc3.edu>
2017-11-07 11:40:30 -0500

    Write to Text File

--- src/main/resources/HelperFunctions/HelperFunctions/WriteToText.java
+++ src/main/resources/HelperFunctions/HelperFunctions/WriteToText.java
@@ -6,5 +6,19 @@
  */
 package HelperFunctions;
 
+import java.io.PrintStream;
+
 public class WriteToText {
+
+    public static void writer(String text, String fileName){
+        try{
+
+            PrintStream makeWriter = new PrintStream(fileName);
+            makeWriter.println(text);
+            makeWriter.close();
+
+        }catch (Exception e){
+            System.out.println("File Not Found.");
+        }
+    }
 }
