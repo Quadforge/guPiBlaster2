d9bbcb8501b5a5736ee1f8bcd6c94e970cfa58f0
LoopyELBARTO <loopyelbarto@gmail.com>
2017-11-10 01:20:22 -0500

    Test gui for to see if reading are being displayed into the textfield

---
+++ src/main/java/views/GUI/GUIDriver.java
@@ -0,0 +1,11 @@
+package GUI;
+
+import com.pi4j.io.i2c.I2CFactory;
+
+import java.io.IOException;
+
+public class GUIDriver {
+    public static void main(String[] args) throws IOException, I2CFactory.UnsupportedBusNumberException {
+        GuPiBlaster main = new GuPiBlaster("guPiBlaster");
+    }
+}
